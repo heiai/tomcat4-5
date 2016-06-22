ef51585766cdee8d2b3393c0912bda88fee1070c
heiai <770756122@qq.com>
2016-06-22 16:35:46 +0800

    tomcat source

---
+++ tomcat-demo/src/main/java/com/slider/tomcat/ex03/startup/Bootstrap.java
@@ -0,0 +1,10 @@
+package com.slider.tomcat.ex03.startup;
+
+import com.slider.tomcat.ex03.connector.http.HttpConnector;
+
+public final class Bootstrap {
+  public static void main(String[] args) {
+    HttpConnector connector = new HttpConnector();
+    connector.start();
+  }
+}
